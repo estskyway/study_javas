73703c89e891e51792965e2b2b0a70fdef05a469
estskyway <estskyway@gmail.com>
2023-05-24 23:44:16 +0900

    11시 44분

---
+++ src/Arrays.java
@@ -0,0 +1,11 @@
+public class Arrays {
+    public static void main(String[] args) {
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+        
+        for (int first = 0; first < 4; first = first + 1) {
+            System.out.println(cars[first]);
+        }
+        System.out.println("end");
+        // return 0;
+    }
+}
