a596237aa10a05f24c6f9042cd327cb8f933a644
estskyway <estskyway@gmail.com>
2023-05-24 16:52:51 +0900

    4시 52분

---
+++ src/LoopNestedLoops.java
@@ -0,0 +1,20 @@
+public class LoopNestedLoops {
+    public static void main(String[] args) {
+
+        for (int first = 1; first < 5; first = first + 1) {
+            System.out.println("for " + first);
+            for (int second = 1; second <= 4; second = second + 1) {
+                
+                if (second <= first) {
+                    System.out.print("_?");
+                }
+                else  {
+                    break;
+                }
+            }
+            System.out.println();
+        }
+        System.out.println("end");
+        // return 0;
+    }
+}
