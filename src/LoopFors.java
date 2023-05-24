93f8f05e704fff9cdd457eed8966053a4d5960e4
estskyway <estskyway@gmail.com>
2023-05-24 15:30:17 +0900

    3시 30분

---
+++ src/LoopFors.java
@@ -0,0 +1,16 @@
+public class LoopFors {
+    public static void main(String[] args) {
+        // for (statement 1; statement 2; statement 3) {
+        //     // code block to be executed
+        //   }
+                    // (While문의 변수선언; 옵션 ; While문 안의 loop조건)
+                    // for문이 처음 작동할때 앞에 두개가 작동하고 그후에 뒤에 두개가 작동한다.
+                int second = 10;
+                    for (int first = 5; first < second; first = first + 2) {
+                    // code block to be executed
+                    System.out.println(first);
+          }
+          System.out.println();
+        // return 0;
+    }
+}
---
+++ src/LoopWithMark.java
@@ -0,0 +1,10 @@
+public class LoopWithMark {
+    public static void main(String[] args) {
+        
+        for (int L = 1 ; L < 5; L = L + 1) {
+            System.out.println("for "+L);
+        }
+        System.out.println("end");
+        // return 0;
+    }
+}
