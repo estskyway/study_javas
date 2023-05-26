61cbf4bb742e55fe6a5f869b146189c0de8a65c2
estskyway <estskyway@gmail.com>
2023-05-26 10:41:43 +0900

    10시 41분

--- src/Arrays.java
+++ src/Arrays.java
@@ -1,7 +1,12 @@
 public class Arrays {
     public static void main(String[] args) {
         String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
-        System.out.println();
+        
+        for (int first = 0; first < 4; first = first + 1) {
+            System.out.println(cars[first]);
+        }
+
+        System.out.println("end");
         // return 0;
     }
 }
---
+++ src/test/main_codingtest.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+    // https://www.acmicpc.net/problem/10998
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
+
+        int A = 0;
+        int B = 0;
+        A = myObj.nextInt();
+        B = myObj.nextInt();
+        System.out.println(A*B);
+        // return 0;
+    }
+}
