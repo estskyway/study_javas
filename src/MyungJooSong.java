d3df803eceb39ea151f25d8cb7dcdf3895339cad
angelamyungjoosong <angelaforever122@gmail.com>
2023-05-31 12:17:20 +0900

    송명주

---
+++ src/MyungJooSong.java
@@ -0,0 +1,11 @@
+public class MyungJooSong {
+    public static void main(String[] args) {
+        try {
+            System.out.println("송명주");
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+        //return 0;
+    }
+    
+}
