3ca9d48851a80ebdbb7e8c938fb02d0512d008db
estskyway <estskyway@gmail.com>
2023-06-09 18:24:14 +0900

    6/918시 24분

---
+++ src/ConnectDBs.java
@@ -0,0 +1,31 @@
+import java.sql.*;
+
+public class ConnectDBs {
+    public static void main(String[] args) {
+        try {
+            // - MySQL workbench 실행 : JDBC
+            // - User/password와 접속 IP:port 접속
+            String url = "jdbc:mysql://localhost:3306/db_cars";
+            String user = "root";
+            String password = "!yojulab*";
+
+            Connection connection = DriverManager.getConnection(url, user, password);
+            System.out.println("DB연결 성공\n");
+
+            // - query Edit
+            Statement statement = connection.createStatement();
+            String quary = "SELECT * FROM factorys";
+            ResultSet resultSet = statement.executeQuery(quary);
+            while(resultSet.next()){
+               
+                System.out.println( 
+                resultSet.getString("COMPANY_ID")
+                +resultSet.getString("COMPANY"));
+            }
+            System.out.println();
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+        // return 0;
+    }
+}
