0454f4bea4af513706a04f82ab6efe40072da763
estskyway <estskyway@gmail.com>
2023-06-15 16:10:57 +0900

    6/15 16시 10분 임거산

---
+++ docs/survey/statistics.sql
@@ -0,0 +1,19 @@
+-- SELECT column_name(s)
+-- FROM table_name
+-- WHERE condition
+-- GROUP BY column_name(s)
+-- HAVING condition
+-- ORDER BY column_name(s)
+-- JOIN
+-- ON;
+
+
+-------- 참조 : poll Statistic example -------------
+-- 총 설문자 : 3명
+SELECT COUNT(*) CNT
+FROM (
+	SELECT RESPONDENTS_ID, COUNT(*) CNT
+	FROM statistics
+	group by RESPONDENTS_ID
+	) AS T_STATIC
+;
