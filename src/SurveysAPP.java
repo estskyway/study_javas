478e5afd4f672d7da79503b8d57d3b3ed95dc458
estskyway <estskyway@gmail.com>
2023-06-16 09:37:12 +0900

    6/16 9시 37분

--- src/SurveysAPP.java
+++ src/SurveysAPP.java
@@ -8,7 +8,7 @@ import surveys.Statistics;
 /**
  * SurveysApp
  */
-public class SurveysApp {
+public class SurveysAPP {
     public static void main(String[] args) {
         try {
             // - MySQL workbench 실행 : JDBC
